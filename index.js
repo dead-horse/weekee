526ac64286474f5c70bc64afcd75fb9e5228ac7e
不四 <busi.hyy@taobao.com>
2013-04-24 11:38:46 +0800

    mv weekee to index.js

--- index.js
+++ index.js
@@ -1,2 +1,47 @@
-module.exports = process.env.WEEKEE_COV ? require('./lib-cov/weekee') : require('./lib/weekee');
+/*!
+ * weekee - weekee.js 
+ * Copyright(c) 2013
+ * Author: dead-horse <dead_horse@qq.com>
+ */
+
+
+/**
+ * Module dependencies.
+ */
+var sio = require('socket.io');
+var ioHandler = require('./lib/io');
+var nodeStatic = require('node-static');
+
+/**
+ * Init a weekee
+ * @param {Object} options 
+ *   - {HttpServer} server         a http server or use connect/express
+ *   - {Number} port               if use the defualt server, the default server will listen this port, defualt is 8080
+ *   - {Function} configSocketIO   a function that set socketIO's config
+ *   - {String} directory          the root directory of wiki
+ * @return {[type]} [description]
+ */
+module.exports = function weekee(options) {
+  options = options || {};
+  var server = options.server;
+  var enableStatic;
+  //http server
+  if (server) {
+    enableStatic = options.enableStatic || true;
+  } else {
+    //default http server. pathname = `/`
+    server = require('./lib/server')(options.port || 8080);
+    enableStatic = true;
+  }
+
+  //enable static file, pathname in `/weekee/assets/`
+  enableStatic && require('./lib/static')(server);
+
+  var io = sio.listen(server);
+
+  var directory = options.directory || process.cwd();
+  options.configSocketIO && options.configSocketIO(io);
+  ioHandler.bind(io, directory);
+};
+
 
