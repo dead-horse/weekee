17f908be9cd678765c0a81d83708c7635a29fcfe
dead-horse <dead_horse@qq.com>
2013-04-25 15:17:45 +0800

    refine

--- index.js
+++ index.js
@@ -8,10 +8,7 @@
 /**
  * Module dependencies.
  */
-var sio = require('socket.io');
-var ioHandler = require('./lib/io');
-var nodeStatic = require('node-static');
-var gitActions = require('./lib/git');
+var weekee =require('./lib/weekee');
 
 /**
  * Init a weekee
@@ -25,38 +22,7 @@ var gitActions = require('./lib/git');
  *   - {String} url            git remote url
  *   - {String} name           git user.name
  *   - {String} email          git user.email
- * @return {[type]} [description]
  */
-module.exports = function weekee(options) {
-  options = options || {};
-  var server = options.server;
-  var enableStatic;
-  var git = options.git || {};
-  var directory = options.directory || process.cwd();
-  if (options.git) {
-    gitActions.init(git, directory, function (err, message) {
-      if (err) {
-        console.error('[INIT GIT ERROR]' + err.message);
-      }
-      if (message) {
-        console.warn('[INIT GIT WARN]' + message);
-      }
-    });
-  }
-  //http server
-  if (server) {
-    enableStatic = options.enableStatic || true;
-  } else {
-    //default http server. pathname = `/`
-    server = require('./lib/server')(options.port || 8080);
-    enableStatic = true;
-  }
-
-  //enable static file, pathname in `/weekee/assets/`
-  enableStatic && require('./lib/static')(server);
-
-  var io = sio.listen(server);
-
-  options.configSocketIO && options.configSocketIO(io);
-  ioHandler.bind(io, directory, git);  
-};
+exports.create = weekee.create;
+exports.RedisStore = require('socket.io/lib/stores/redis');
+exports.redis = require('socket.io/node_modules/redis');
